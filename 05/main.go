a0e042b24f6c940ce266b9ab430578800735ecb9
Саша Воробьёв <avorobiev@deloitte.ru>
2020-06-29 23:27:36 +0300

    added type conversion to challenge myself

--- 05/main.go
+++ 05/main.go
@@ -4,18 +4,42 @@ import (
 	"encoding/csv"
 	"log"
 	"os"
+	"strconv"
 	"text/template"
+	"time"
 )
 
 var tmp *template.Template
 
-func init() {
-	var err error
-	tmp, err = template.ParseGlob("./*.gohtml")
+var fm = template.FuncMap{
+	"convdate":  convertdate,
+	"convfloat": convertfloat,
+}
+
+func convertdate(s string) time.Time {
+	res, err := time.Parse("2006-01-02", s)
 
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
+
+	return res
+}
+
+func convertfloat(s string) float64 {
+	res, err := strconv.ParseFloat(s, 64)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	return res
+}
+
+func init() {
+
+	tmp = template.Must(template.New("").Funcs(fm).ParseFiles("forex.gohtml"))
+
 }
 
 func main() {
